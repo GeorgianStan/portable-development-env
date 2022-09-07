0b91cf1482e6a26b8e4ff4474806b21a7d7c8c17
GeorgianStan <georgian.stan8@gmail.com>
2022-09-07 12:18:22 +0300

    feat: add shell.nix with NodeJS support

---
+++ nixpkgs/shell.nix
@@ -0,0 +1,12 @@
+with (import <nixpkgs> {});
+mkShell {
+  buildInputs = [
+    nodejs-16_x
+  ];
+  shellHook = ''
+      mkdir -p .nix-node
+      export NODE_PATH=$PWD/.nix-node
+      export NPM_CONFIG_PREFIX=$PWD/.nix-node
+      export PATH=$NODE_PATH/bin:$PATH
+  '';
+}
